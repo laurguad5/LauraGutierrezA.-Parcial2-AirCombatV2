7c53e0da9c26de3a8329c00978d4a857936ceb40
laurguad5 <36015596+laurguad5@users.noreply.github.com>
2018-04-18 23:03:11 -0500

    AirCombat
    Destruir alieens

---
+++ LauraGutierrezA.-AirCombatV2/Assets/Scripts/MissileFalling.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileFalling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Mover hacia adelante 
+		this.transform.Translate(0.0f, 0.0f, 0.7f);
+	}
+	void OnCollisionEnter(Collision collision)  { 
+
+		//Cargamos el prefab de la explosion 
+		GameObject explosion = GameObject.Instantiate (
+			Resources.Load ("Prefabs/Explosion") as GameObject); 
+
+		//Muevo la explosion hacia la nave que fue impactada
+		explosion.transform.position = collision.collider.gameObject.transform.position;
+
+		//(gameobject con TAG MarsAlien)
+		if (collision.collider.gameObject.tag == "MarsAlien") { //Los {} Son importsntes porque la instruccion IF necesita saber qué tiene que hacer. 
+			                                                //Lo que esta adentro de los corchetes será lo  que hará la instruccion. 
+		
+			//Destruimos el objeto impactado por el misil 
+			Destroy (collision.collider.gameObject);
+
+			//Destruimos el misil
+			Destroy (this.gameObject);
+	
+
+		
+}
+		//(gameobject con TAG MarsAlien)
+		if (collision.collider.gameObject.tag == "JupiterAlien") { //Los {} Son importsntes porque la instruccion IF necesita saber qué tiene que hacer. 
+			//Lo que esta adentro de los corchetes será lo  que hará la instruccion. 
+
+			//Destruimos el objeto impactado por el misil 
+			Destroy (collision.collider.gameObject);
+
+			//Destruimos el misil
+			Destroy (this.gameObject);
+
+	}
+}
+}
---
+++ LauraGutierrezA.-AirCombatV2/Assets/Scripts/Translate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Translate (0.0f, 0.0f, 0.3f);
+	}
+}
